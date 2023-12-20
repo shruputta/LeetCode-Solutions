5bf44f9ec9a86d1c8602d45dfc0c5b8e6f07f408
shruthi <shruthi.hshruthii@gmail.com>
2023-12-20 22:38:21 +0530

    1484. Group Sold Products By The Date.sql

---
+++ 1484. Group Sold Products By The Date.sql
@@ -0,0 +1,37 @@
+Input: 
+Activities table:
++------------+------------+
+| sell_date  | product     |
++------------+------------+
+| 2020-05-30 | Headphone  |
+| 2020-06-01 | Pencil     |
+| 2020-06-02 | Mask       |
+| 2020-05-30 | Basketball |
+| 2020-06-01 | Bible      |
+| 2020-06-02 | Mask       |
+| 2020-05-30 | T-Shirt    |
++------------+------------+
+Output: 
++------------+----------+------------------------------+
+| sell_date  | num_sold | products                     |
++------------+----------+------------------------------+
+| 2020-05-30 | 3        | Basketball,Headphone,T-shirt |
+| 2020-06-01 | 2        | Bible,Pencil                 |
+| 2020-06-02 | 1        | Mask                         |
++------------+----------+------------------------------+
+Explanation: 
+For 2020-05-30, Sold items were (Headphone, Basketball, T-shirt), we sort them lexicographically and separate them by a comma.
+For 2020-06-01, Sold items were (Pencil, Bible), we sort them lexicographically and separate them by a comma.
+For 2020-06-02, the Sold item is (Mask), we just return it.
+
+
+
+soln:
+  
+  select sell_date ,count(sell_date) as num_sold,
+String_AGG(product,',' ) WITHIN GROUP (ORDER BY product) AS products                   
+from 
+(select 
+DISTINCT  sell_date, product
+from Activities ) as dup
+group by sell_date
